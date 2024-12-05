fb7e4dd2c93e054e3fb35e9280c5bc6670e8280f
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-12-05 04:05:38 +0100

    fancy app menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,16 @@ import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-
+import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterLink,
     RouterOutlet,
-    WelcomeComponent,
-    PersonTableComponent,
-    EventBindExampleComponent
+    ListGroupMenuComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
