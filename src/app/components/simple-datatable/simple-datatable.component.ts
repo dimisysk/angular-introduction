b2aa660f56fb0e8f94d091875886f3e9a0a8a06d
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-12-07 00:03:09 +0100

    simple data table

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,5 +1,7 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component, Input } from '@angular/core';
 import { EPerson, Person } from 'src/app/shared/interfaces/person';
+import { sortBy } from 'lodash-es';
 
 @Component({
   selector: 'app-simple-datatable',
@@ -10,4 +12,38 @@ import { EPerson, Person } from 'src/app/shared/interfaces/person';
 })
 export class SimpleDatatableComponent {
   @Input() data: EPerson[];
+
+  sortOrder = {
+    givenName: 'none',
+    surName: 'none',
+    age: 'none',
+    email: 'none',
+    education: 'none',
+  };
+
+  sortData(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc') {
+      this.sortOrder[sortKey] = 'desc';
+      this.data = sortBy(this.data, sortKey).reverse();
+    } else {
+      this.sortOrder[sortKey] = 'asc';
+      this.data = sortBy(this.data, sortKey);
+    }
+    for (let key in this.sortOrder) {
+      if (key !== sortKey) {
+        this.sortOrder[key] = 'none'
+      }    
+    }
+  }
+
+  sortSign(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc') {
+      return '↑';
+    } else if (this.sortOrder[sortKey] === 'desc') {
+      return '↓';
+    } else {
+      return '';
+    }
+  }
+  
 }
