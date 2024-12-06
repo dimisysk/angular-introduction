be7bc661cdcbe8690f458ab64cc32a018fe2ce36
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-12-06 05:42:38 +0100

    simple datatable primer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,20 @@
 import { Routes } from '@angular/router';
-import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component'
+import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
 import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
+import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 
 export const routes: Routes = [
-    {path: 'event-bind-example', component: EventBindExampleComponent },
-    {path: '', component: WelcomeComponent},
-    {path: 'component-input-example', component: ComponentInputExampleComponent},
-    {path: 'for-directive-examle', component: ForDirectiveExampleComponent}
+  { path: 'event-bind-example', component: EventBindExampleComponent },
+  { path: '', component: WelcomeComponent },
+  {
+    path: 'component-input-example',
+    component: ComponentInputExampleComponent,
+  },
+  { path: 'for-directive-example', component: ForDirectiveExampleComponent },
+  {
+    path: 'simple-datatable-example',
+    component: SimpleDatatableExampleComponent,
+  },
 ];
