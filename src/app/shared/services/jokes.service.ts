a3d6d8ae5fc41a9cae7b15503e63a7c5467c35e9
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-12-13 02:49:54 +0100

    http client primer

---
+++ src/app/shared/services/jokes.service.ts
@@ -0,0 +1,13 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const DAD_JOKES_API_URL = 'https://icanhazdadjoke.com/';
+const JACK_NORRIS_JOKES_API_URL = 'https://api.chucknorris.io/jokes/random';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JokesService {
+
+  http: HttpClient = inject(HttpClient)
+}
